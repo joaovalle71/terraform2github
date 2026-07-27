478e26bc4b1e1d808c590b65aa8ad760e1be4263
joaovalle <joaovalle@hotmail.com>
2026-07-27 19:03:58 -0300

    correcoes

--- extract.sh
+++ extract.sh
@@ -52,13 +52,12 @@ if [[ $(echo "$result"|grep -c "var_repo_error=") -gt 0 ]]; then
     echo "$result"
     exit 1
 fi
+
 if [[ "$DEBUG" == "true" ]]; then
     echo "$result"
-    echo "GITHUB_REPOSITORY_TYPE=${repo_source_owner_type}"
 else
     echo "$result"|grep -E "^var_user_login"
 fi
-
 
 if [[ "${repo_source_owner_type}" == "User" ]]; then
     export GITHUB_REPOSITORY_TYPE="users"
